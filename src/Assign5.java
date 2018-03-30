f114ee545aaefac5cfe703e7a2e495c03a5889b8
Jonathan Yee <webber989@gmail.com>
2018-03-29 23:24:47 -0600

    Added initial program files. Added commandline verification code block in the main java file

---
+++ src/Assign5.java
@@ -0,0 +1,19 @@
+
+public class Assign5 {
+
+	public static void main(String[] args) {
+		//Command line argument verification 
+				/*
+				if(args.length != 4) {
+					System.out.println("Incorrect number of inputs. Quitting...");
+					System.exit(-1);
+				}
+				if(!getFileExtension(args[0]).equals(".txt") || !getFileExtension(args[1]).equals(".txt") || !getFileExtension(args[2]).equals(".txt")
+						|| !getFileExtension(args[3]).equals(".txt") ) {
+					System.out.println("Unable to use files that are not text files. Check your file names. Quitting...");
+					System.exit(-1);
+				}*/
+
+	}
+
+}
